6700a03df3551021572f116eea5a7d16281e6daa
TITO <brandtitouan@gmail.com>
2024-06-14 10:50:15 +0200

    teste css

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-// echo "ii";
 
 /**
  * Front controller
--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install docker -y
-sudo usermod -a -G docker ec2-user
-wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
-sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
-sudo chmod -v +x /usr/local/bin/docker-compose
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
+# sudo yum update -y
+# sudo yum install docker -y
+# sudo usermod -a -G docker ec2-user
+# wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+# sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+# sudo chmod -v +x /usr/local/bin/docker-compose
+# sudo systemctl enable docker.service
+# sudo systemctl start docker.service
 
 # git clone
 # lance docker compose
