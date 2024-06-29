883f5658f2aa317baccc45b0969a4cf3349f727a
TITO <brandtitouan@gmail.com>
2024-06-29 16:02:01 +0200

    swagger + view

--- App/Controllers/Api.php
+++ App/Controllers/Api.php
@@ -9,6 +9,7 @@ use Exception;
 
 /**
  * API controller
+ * @OA\Info(title="LoftCube API", version="0.1")
  */
 class Api extends \Core\Controller
 {
@@ -17,28 +18,56 @@ class Api extends \Core\Controller
      * Affiche la liste des articles / produits pour la page d'accueil
      *
      * @throws Exception
+     *
+     * @OA\Get(
+     *     path="/Api/Products?sort={sort}",
+     *     summary="Récupérer tous les produits",
+     *     tags={"Gets"},
+     *     @OA\Parameter(description="Trie ('views' ou 'date')", name="sort", in="path"),
+     *     @OA\Response(response="200", description="La récupération des données est correct"),
+     *     @OA\Response(response="404", description="Impossible de récupérer les données")
+     *
+     * )
      */
     public function ProductsAction()
     {
-        $query = $_GET['sort'];
+        try {
+            $query = $_GET['sort'];
 
-        $articles = Articles::getAll($query);
+            $articles = Articles::getAll($query);
 
-        header('Content-Type: application/json');
-        echo json_encode($articles);
+            header('Content-Type: application/json');
+            echo json_encode($articles);
+        } catch (\Exception $e) {
+
+            echo "<script>console.log('Debug Objects: " . $e . "' );</script>";
+        }
     }
 
     /**
      * Recherche dans la liste des villes
      *
      * @throws Exception
+     *
+     * @OA\Get(
+     *     path="/Api/Cities",
+     *     summary="Récupérer toutes les villes",
+     *     tags={"Gets"},
+     *     @OA\Response(response="200", description="La récupération des données est correct"),
+     *     @OA\Response(response="404", description="Impossible de récupérer les données")
+     *
+     * )
      */
     public function CitiesAction()
     {
+        try {
+            $cities = Cities::search($_GET['query']);
 
-        $cities = Cities::search($_GET['query']);
+            header('Content-Type: application/json');
+            echo json_encode($cities);
+        } catch (\Exception $e) {
 
-        header('Content-Type: application/json');
-        echo json_encode($cities);
+            echo "<script>console.log('Debug Objects: " . $e . "' );</script>";
+        }
     }
 }
