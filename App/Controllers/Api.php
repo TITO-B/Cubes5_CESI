640891fc4977d083e2a4f62d6c1966e229609acd
vincouille <condellovince2004@gmail.com>
2024-07-01 00:28:08 +0200

    début correction page add

--- App/Controllers/Api.php
+++ App/Controllers/Api.php
@@ -8,50 +8,66 @@ use \Core\View;
 use Exception;
 
 /**
- * @OA\Info(title="API Vide Grenier", version="1.0")
+ * API controller
+ * @OA\Info(title="LoftCube API", version="0.1")
  */
 class Api extends \Core\Controller
 {
 
     /**
-     * @OA\Get(
-     *     path="/Api/Products",
-     *     summary="Affiche tous les produits",
-     *     tags={"Products"},
-     *     @OA\Response(response="200", description="An example resource"),
-     *     @OA\Response(response="404", description="Not found"),
-     * )
      * Affiche la liste des articles / produits pour la page d'accueil
      *
      * @throws Exception
+     *
+     * @OA\Get(
+     *     path="/Api/Products?sort={sort}",
+     *     summary="Récupérer tous les produits",
+     *     tags={"Gets"},
+     *     @OA\Parameter(description="Trie ('views' ou 'date')", name="sort", in="path"),
+     *     @OA\Response(response="200", description="La récupération des données est correct"),
+     *     @OA\Response(response="404", description="Impossible de récupérer les données")
+     *
+     * )
      */
     public function ProductsAction()
     {
-        $query = $_GET['sort'];
+        try {
+            $query = $_GET['sort'];
 
-        $articles = Articles::getAll($query);
+            $articles = Articles::getAll($query);
 
-        header('Content-Type: application/json');
-        echo json_encode($articles);
+            header('Content-Type: application/json');
+            echo json_encode($articles);
+        } catch (\Exception $e) {
+
+            echo "<script>console.log('Debug Objects: " . $e . "' );</script>";
+        }
     }
 
     /**
-     * @OA\Get(
-     *     path="Api/Cities",
-     *     summary="Affiche tous les produits",
-     *     tags={"Cities"},
-     *     @OA\Response(response="200", description="An example resource"),
-     *     @OA\Response(response="404", description="Not found"),
-     * )
      * Recherche dans la liste des villes
      *
      * @throws Exception
+     *
+     * @OA\Get(
+     *     path="/Api/Cities",
+     *     summary="Récupérer toutes les villes",
+     *     tags={"Gets"},
+     *     @OA\Response(response="200", description="La récupération des données est correct"),
+     *     @OA\Response(response="404", description="Impossible de récupérer les données")
+     *
+     * )
      */
-    public function CitiesAction(){
+    public function CitiesAction()
+    {
+        try {
+            $cities = Cities::search($_GET['query']);
 
-        $cities = Cities::search($_GET['query']);
+            header('Content-Type: application/json');
+            echo json_encode($cities);
+        } catch (\Exception $e) {
 
-        header('Content-Type: application/json');
-        echo json_encode($cities);
+            echo "<script>console.log('Debug Objects: " . $e . "' );</script>";
+        }
     }
 }
--- App/Utility/Hash.php
+++ App/Utility/Hash.php
@@ -5,32 +5,38 @@ namespace App\Utility;
 /**
  * Hash:
  */
-class Hash {
+class Hash
+{
 
     /**
      * Génère et retourne un hash
      */
-    public static function generate($string, $salt = "") {
-        return(hash("sha256", $string . $salt));
+    public static function generate($string, $salt = "")
+    {
+        return (hash("sha256", $string . $salt));
     }
 
     /**
      * Génère et retourne un salt
      */
-    public static function generateSalt($length) {
+    public static function generateSalt($length)
+    {
+
+
         $salt = "";
         $charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/\\][{}\'\";:?.>,<!@#$%^&*()-_=+|";
         for ($i = 0; $i < $length; $i++) {
             $salt .= $charset[mt_rand(0, strlen($charset) - 1)];
         }
+
         return $salt;
     }
 
     /**
      * Génère et retourne un UID
      */
-    public static function generateUnique() {
-        return(self::generate(uniqid()));
+    public static function generateUnique()
+    {
+        return (self::generate(uniqid()));
     }
-
 }
