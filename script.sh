9d0d720e3c4399325e24ff9f6c418c35b8fa19a6
vincouille <condellovince2004@gmail.com>
2024-07-09 10:56:45 +0200

    v1 du script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+# Met à jour tous les paquets installés sur le système à leur dernière version disponible
 sudo yum update -y
+
+# Installe Docker, l'outil de conteneurisation
 sudo yum install docker -y
+
+# Ajoute l'utilisateur 'ec2-user' au groupe 'docker' pour permettre l'exécution des commandes Docker sans 'sudo'
 sudo usermod -a -G docker ec2-user
-wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+
+# Télécharge la version 3.7 de Docker Compose depuis GitHub, adaptée au système d'exploitation et à l'architecture de la machine
+wget https://github.com/docker/compose/releases/download/v3.7/docker-compose-$(uname -s)-$(uname -m)
+
+# Déplace le fichier téléchargé de Docker Compose vers le répertoire /usr/local/bin et le renomme en 'docker-compose'
 sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+
+# Rend le fichier Docker Compose exécutable
 sudo chmod -v +x /usr/local/bin/docker-compose
+
+# Configure Docker pour démarrer automatiquement au démarrage du système
 sudo systemctl enable docker.service
+
+# Démarre le service Docker immédiatement
 sudo systemctl start docker.service
+
 
 # git clone
 # lance docker compose
